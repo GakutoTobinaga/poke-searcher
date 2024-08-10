c5782a6f84c10808a6675cd4b39e914876d295f5
gtobinaga-sparkleai <gtobinaga@sparkleai.co.jp>
2024-08-11 00:10:20 +0900

    add: Add Loading sample component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
 import Pokemon from './components/pokemon';
-import Loading from './components/loading';
-import { Suspense } from 'react';
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<Loading />}>
-        <Pokemon />
-      </Suspense>
+      <Pokemon/>
     </div>
   );
 }
--- src/components/pokemon.jsx
+++ src/components/pokemon.jsx
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Loading from './loading';
 const Pokemon = () => {
   const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchPokemon = async () => {
       try {
-        const randomInt = Math.floor(Math.random() * 1010)
-        console.log(randomInt)
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomInt}`);
-        setPokemon({
-          name: response.data.name,
-          imageUrl: response.data.sprites.front_default
-        });
+        setTimeout(async () => {
+          const randomInt = Math.floor(Math.random() * 1010);
+          console.log(randomInt);
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomInt}`);
+          setPokemon({
+            name: response.data.name,
+            imageUrl: response.data.sprites.front_default
+          });
+          setLoading(false);
+        }, 5000); // 5秒の遅延を追加
       } catch (error) {
         console.error('Error fetching Pokemon data:', error);
+        setLoading(false);
       }
     };
 
     fetchPokemon();
   }, []);
+
+  if (loading) {
+    return <Loading/>
+  }
+
   return (
     <div>
       {pokemon && (
